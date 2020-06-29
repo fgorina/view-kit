52ce3fc0e0defd62581daad2d48b3e7a78ac52ed
Francisco Gorina <fgorina@gmail.com>
2020-06-29 13:37:10 +0200

    Changed Initializer

--- Sources/ViewKit/Context/ListContext.swift
+++ Sources/ViewKit/Context/ListContext.swift
@@ -9,10 +9,11 @@
 public struct ListContext<T: Encodable>: Encodable {
     /// encodable list property
     
-    public var menu : [MenuItem] = []
+    public var menu : [MenuItem]
     public var list: [T]
     
-    public init(_ list: [T]) {
+    public init(_ list: [T], menu: [MenuItem] = []) {
         self.list = list
+        self.menu = menu
     }
 }
