d82e0f1b438b1e096b7cb8ba9c04dfcd5671ae20
Francisco Gorina <fgorina@gmail.com>
2020-06-29 13:32:03 +0200

    Added a public init
    If not it  may not be accessed from outside the package

--- Sources/ViewKit/Context/MenuItem.swift
+++ Sources/ViewKit/Context/MenuItem.swift
@@ -5,4 +5,10 @@ public struct MenuItem : Encodable {
     public var title : String
     public var url : String
     public var position : Int
+    
+    public init(title: String, url: String, position : Int){
+        selt.title = title
+        self.url = url
+        self.position = position
+    }
 }
