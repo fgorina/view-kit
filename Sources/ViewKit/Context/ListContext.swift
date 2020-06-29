0b84a1e2bc86c6349fb0fb1548d21c7ab4ac791c
Francisco Gorina <fgorina@gmail.com>
2020-06-29 13:12:13 +0200

    Added menu variable
    Menu variable contains an array of MenuItem's to be shown in the view

--- Sources/ViewKit/Context/ListContext.swift
+++ Sources/ViewKit/Context/ListContext.swift
@@ -8,6 +8,8 @@
 /// a generic list context for rendering list items
 public struct ListContext<T: Encodable>: Encodable {
     /// encodable list property
+    
+    public var menu : [MenuItem]
     public var list: [T]
     
     public init(_ list: [T]) {
