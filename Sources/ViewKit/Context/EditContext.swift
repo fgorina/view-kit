bba82ab4b8a5f64bf83d7f0cabff28ed886d70d5
Francisco Gorina <fgorina@gmail.com>
2020-06-29 13:36:28 +0200

    Changed Initializer

--- Sources/ViewKit/Context/EditContext.swift
+++ Sources/ViewKit/Context/EditContext.swift
@@ -8,12 +8,13 @@
 /// a generic edit context for rendering forms
 public struct EditContext<T: Encodable>: Encodable {
     
-    public var menu : [MenuItem] = []
+    public var menu : [MenuItem] 
     /// encodable edit property
     public var edit: T
     
-    public init(_ edit: T) {
+    public init(_ edit: T, menu: [MenuItem] = []) {
         self.edit = edit
+        self.menu = menu
     }
 }
 
